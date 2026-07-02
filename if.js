d0b8f01eb04c0602e8ed636769d014d87b5e719d
Sima-akhter <simaakter5301@gmail.com>
2026-07-02 22:11:05 +0600

    update if file

--- if.js
+++ if.js
@@ -1,8 +1,14 @@
 const salary = 25000;
 const isBCS = true;
+const height = 68;
 
-if (salary > 20000 ) { 
-    console.log("yeas");
-} else {
+// if (salary > 20000 && isBCS) {
+//     console.log("yeas");
+// } else {
+//     console.log("no");
+// }
+if (salary > 25000 || height > 60) {
+    console.log("yes");
+}else {
     console.log("no");
 }
